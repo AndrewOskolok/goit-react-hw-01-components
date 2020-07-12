01956da3735c438913d5606813de136c2f9d2b20
Oscolok <oscolok7@gmail.com>
2020-07-12 17:04:10 +0300

    Updates

---
+++ precache-manifest.4b1a2ac1dec8c6d751496f71cbe33aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2a44747d1176b7eb92aa0663a5604d",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "5d06601458362db430c7",
+    "url": "/goit-react-hw-01-components/static/css/main.cd124004.chunk.css"
+  },
+  {
+    "revision": "873e5f19fb47b3df99cc",
+    "url": "/goit-react-hw-01-components/static/js/2.fa41f1f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.fa41f1f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d06601458362db430c7",
+    "url": "/goit-react-hw-01-components/static/js/main.2b72664f.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
